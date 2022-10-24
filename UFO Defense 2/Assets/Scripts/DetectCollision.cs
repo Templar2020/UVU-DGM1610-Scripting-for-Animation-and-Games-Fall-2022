7c718c353231813be404a1787ef647c662c0f12c
Preston Powell <preston.powell2020@gmail.com>
2022-10-24 17:17:56 -0600

    Finished UFO game and Started 2D Plaformer
    - Added 2D player controller
    - Added Enemy Patrol
    - Platform effectors

--- UFO Defense 2/Assets/Scripts/DetectCollision.cs
+++ UFO Defense 2/Assets/Scripts/DetectCollision.cs
@@ -6,19 +6,28 @@ public class DetectCollision : MonoBehaviour
 {
 
     private ScoreManager scoreManager; // A variable to hold the reference to the scoremanager
-    public int scoreToGive; 
+    public int scoreToGive;
+    public ParticleSystem explosionParticle; // Store the particle system
 
 
     // Start is called before the first frame update
     void Start()
     {
         scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Reference scoremanager
+        
     }
 
     void OnTriggerEnter(Collider other)
     {
+        Explosion();
         scoreManager.IncreaseScore(scoreToGive); // Increase Score 
         Destroy(other.gameObject); //Destroy the other game object it hits
-        Destroy(gameObject); // Destroy this game object        
+        Destroy(gameObject); // Destroy this game object 
+        
+    }
+
+  void Explosion()
+    {
+        Instantiate(explosionParticle, transform.position, transform.rotation);        
     }
 }
